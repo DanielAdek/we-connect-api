ab87807933c22ce5e2f4d34feeb47f6827faf0eb
DanielAdek <maildaniel.me1@gmail.com>
2018-11-08 01:15:31 +0100

    ft (business) user can update business
    -update endpoint
    
    [Delivers #161784272]

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,9 @@ import businessController from '../controllers/businessController';
 
 const router = express.Router();
 
-const { createBusiness } = businessController;
+const { createBusiness, modifyBusiness } = businessController;
 
 router.post('/business/create', createBusiness);
+router.put('/business/:businessId', modifyBusiness);
 
 export default router;
